a4dc38e1501a5a98a1643b8f81d728bfe88d8791
dlepich <d.lepich@fireup.pro>
2025-03-28 16:21:11 +0100

    Simplify Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
 export default defineConfig({
   base: '/songs/',
@@ -11,19 +10,5 @@ export default defineConfig({
       localsConvention: 'camelCase'
     }
   },
-  publicDir: 'public',
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-      },
-      output: {
-        assetFileNames: (assetInfo) => {
-          if (assetInfo.name?.endsWith('.yaml')) {
-            return '[name][extname]'
-          }
-        }
-      }
-    }
-  }
+  publicDir: 'public'
 })
