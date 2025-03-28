67e7270b24add6ceabcb11c6e94d1781a3f02a20
darekpiano <125406306+darekpiano@users.noreply.github.com>
2025-03-28 16:06:32 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import { SongList } from './components/SongList/SongList'
 import SongDetail from './components/SongDetail/SongDetail'
 import './App.css'
